b6f3bc78f8075af487dcd312fe39e60c3b3643de
Kayden Wilkinson <37636922+KuraLight@users.noreply.github.com>
2019-03-10 05:29:05 -0400

    Added test scene with physics and walking animation

---
+++ Elemental Wanderer/Assets/Move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [Range(-1, 1)]
+    public float delta_x;
+
+    [Range(0, 2 * Mathf.PI)]
+    public float phi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        delta_x = .3f;
+        phi = 2 * Mathf.PI / 20;
+        Debug.Log("began");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 7.3 || transform.position.x < -7.3)
+            delta_x *= -1;
+        
+        transform.Translate(delta_x, 0, 0);
+        transform.Rotate(new Vector3(phi, 0, 0));
+        Debug.Log(transform.position.x + ", " + transform.position.y + ", " + transform.position.z);
+    }
+}
---
+++ Elemental Wanderer/Assets/PlayerMove.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Animator anim;
+    SpriteRenderer rend;
+    Rigidbody2D body;
+
+    [Range(-5, 5)]
+    public float walkingSpeed;
+
+    public float jumpForce = 3;
+
+    bool rightPressed;
+    bool leftPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        walkingSpeed = 3f;
+
+        anim = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            body.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetBool("Walking", true);
+            rend.flipX = true;
+            rightPressed = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetBool("Walking", true);
+            rend.flipX = false;
+            leftPressed = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+            rightPressed = false;
+
+        if (Input.GetKeyUp(KeyCode.A))
+            leftPressed = false;
+
+        if(!rightPressed && !leftPressed)
+            anim.SetBool("Walking", false);
+    }
+
+    private void FixedUpdate()
+    {
+        if (anim.GetBool("Walking"))
+        {
+            body.velocity = new Vector2(walkingSpeed * (rend.flipX ? 1 : -1), body.velocity.y);
+        }
+        else
+            body.velocity = new Vector2(0, body.velocity.y);
+
+        Debug.Log(body.velocity.x + " " + body.velocity.y);
+    }
+}
